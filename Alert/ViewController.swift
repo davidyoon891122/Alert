4d4f8912e8aa0e35c00d740e51cbf0f8f194f069
davidyoon <qqpo12@naver.com>
2021-07-01 22:29:55 +0900

    add layout

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var onButton: UIButton!
+    @IBOutlet var offButton: UIButton!
+    @IBOutlet var delButton: UIButton!
+    
+    let onImg = UIImage(named: "lamp-on.png")
+    let offImg = UIImage(named: "lamp-off.png")
+    let delImg = UIImage(named: "lamp-remove.png")
+    var isLampOn = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = onImg
+        
     }
 
 
